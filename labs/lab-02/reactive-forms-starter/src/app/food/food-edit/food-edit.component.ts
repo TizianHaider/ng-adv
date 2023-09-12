49b6114a802a0a05deca1c3c9cb7739710bf1080
Tizian <tizian.haider@gmx.at>
2023-09-12 12:47:06 +0200

    lab-02 part2;

--- labs/lab-02/reactive-forms-starter/src/app/food/food-edit/food-edit.component.ts
+++ labs/lab-02/reactive-forms-starter/src/app/food/food-edit/food-edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, inject } from '@angular/core';
 import { FoodItem } from '../food.model';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
@@ -17,14 +17,49 @@ import { MatCardModule } from '@angular/material/card';
         MatInputModule,
         FormsModule,
         MatButtonModule,
+        ReactiveFormsModule,
     ],
 })
 export class FoodEditComponent {
+
   @Input() food = new FoodItem();
   @Output() onFoodSaved: EventEmitter<FoodItem> = new EventEmitter<FoodItem>();
+  
+  fb = inject(FormBuilder);
 
-  saveFood() {
-    console.log('food to save', this.food);
-    this.onFoodSaved.emit(this.food);
+  // foodForm = this.fb.group({
+  //   id: new FormControl<number>(this.food.id, {
+  //     validators: [Validators.required],
+  //     nonNullable: true
+  //   }),
+  //   name: new FormControl<string>(this.food.name, {
+  //     validators: [Validators.minLength(5)],
+  //     nonNullable: true
+  //   }),
+  //   price: new FormControl<number>(this.food.price, {
+  //     validators: [Validators.min(3)],
+  //     nonNullable: true
+  //   }),
+  //   calories: new FormControl<number>(this.food.calories, {
+  //     nonNullable: true
+  //   }),
+  // });
+
+  foodForm = this.fb.group({
+    id: this.food.id,
+    name: [this.food.name, [Validators.required, Validators.minLength(5)]],
+    price: [this.food.price, [Validators.required, Validators.min(3)]],
+    calories: this.food.calories,
+  });
+
+  saveForm(): void {
+    console.log('food to save', this.foodForm.value);
+    this.onFoodSaved.emit(this.foodForm.value as FoodItem);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['food']) {
+      this.foodForm.setValue(changes['food'].currentValue);
+    }
   }
 }
