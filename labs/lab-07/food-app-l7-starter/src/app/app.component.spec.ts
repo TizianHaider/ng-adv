c22fde5b7235ea1f1b75a29509b82ec79730c442
Tizian <tizian.haider@gmx.at>
2023-09-14 15:32:52 +0200

    lab-07;

--- labs/lab-07/food-app-l7-starter/src/app/app.component.spec.ts
+++ labs/lab-07/food-app-l7-starter/src/app/app.component.spec.ts
@@ -1,13 +1,45 @@
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatCardModule } from '@angular/material/card';
 
 describe('AppComponent', () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        MatSidenavModule,
+        NoopAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatListModule,
+        MatCardModule
+      ],
+      declarations: [
+        AppComponent,
+        NavbarComponent,
+        SidemenuComponent],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
-    pending();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Food App'`, () => {
-    pending();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Food App');
   });
 });
